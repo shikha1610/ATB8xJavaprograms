414b7f32ee6746e20c3feeb644a67abb92e8a0f8
Shikha <shikha1325@gmail.com>
2024-09-06 23:36:21 +0530

    practice

--- src/sept/ex_04092024/Lab003.java
+++ src/sept/ex_04092024/Lab003.java
@@ -1,4 +1,8 @@
 package sept.ex_04092024;
 
 public class Lab003 {
-}
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println(2+2);
+    }
+    }
